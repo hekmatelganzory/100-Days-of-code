5fa5f298eea8e6eeecd5a650b124b847b8a19c10
Hekmat Elganzory <hekmat.elganzory30@gmail.com>
2024-11-26 15:06:29 +0200

    Day one of 100 days of python

---
+++ Pand Name.py
@@ -0,0 +1,8 @@
+print("Welcome to the Band Name Generator.")
+twon_name = input("What's the name of the city you grew up in?")
+pet_name = input("What's your pet's name?")
+
+print("Your band name could be " + twon_name + " " + pet_name)
+# added comment
+_____________
+****
